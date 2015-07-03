48593f40b6b6e5805df726faff5d9b24c47f6a64
fzalkow <fzalkow@reger-max.mri.intern>
2015-07-03 15:34:49 +0200

    small adjustments in script and readme written

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # IMG4CameraRAW
+
+## Description
+
+Some images cannot be opened with Adobe Camera Raw (Adobe CS3). For those cases this small script prepares the images to be opened with Camera Raw. See *usage* for some details what is done.
+
+## Usage
+
+    usage: ready-for-bridge.py [-h] -i INPUT -o OUTPUT [-b]
+
+    Convertes images to to be ready for Camera Raw:
+        1. Deconstructes all layers and chooses the one with biggest file size (so preview layers will be ignored)
+        2. removes all layers aka flatten the image
+        3. removes the alpha cannel
+        4. writes Exif.Photo Dimension metadata to the image)
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -i INPUT, --input INPUT
+                            input file/folder to be converted
+      -o OUTPUT, --output OUTPUT
+                            output file/folder to be written
+      -b, --batch           input and output are folders -- batch convert all files (default: convert single file)
+
+## Dependencies
+
+* [Python 2](https://www.python.org/)
+* [ImageMagick](http://www.imagemagick.org)
+* [exiv2](http://www.exiv2.org)
+* [sips](https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/sips.1.html)
+
+## License
+
+This is MIT license so you can do what you want with the code, just give proper attribution. Anyway, if you make any improvements to the code we are happy if you share it with us or even contribute directly to our project. Please contact rwa@max-reger-institut.de.
--- img4cr.py
+++ img4cr.py
@@ -14,7 +14,7 @@ import shutil
 def convert(infile, outfile):
 	tmp_dir = tempfile.mkdtemp()
 	
-	subprocess.call("convert %s -deconstruct %s/test%%d.tif" % (infile, tmp_dir), shell=True)
+	call("convert %s -deconstruct %s/test%%d.tif" % (infile, tmp_dir))
 	deconstructed_files = glob.glob(tmp_dir + "/*")
 	
 	maxsize = 0
@@ -27,39 +27,50 @@ def convert(infile, outfile):
 			
 	tmpfile = deconstructed_files[maxidx]
 
-	identify = subprocess.check_output("sips -g pixelHeight -g pixelWidth %s" % (tmpfile, ), shell=True)
+	identify = call("sips -g pixelHeight -g pixelWidth %s" % (tmpfile, ))
 	identify = identify.split("\n")
 	x = identify[2].split(" ")[-1]
 	y = identify[1].split(" ")[-1]
 	
-	subprocess.call("convert %s -flatten -alpha off %s" % (tmpfile, outfile), shell=True)
-	subprocess.call('exiv2 -M\"set Exif.Photo.PixelXDimension Long %s\" -M\"set Exif.Photo.PixelYDimension Long %s\" %s' % (x, y, outfile), shell=True)
+	call("convert %s -flatten -alpha off %s" % (tmpfile, outfile))
+	call('exiv2 -M\"set Exif.Photo.PixelXDimension Long %s\" -M\"set Exif.Photo.PixelYDimension Long %s\" %s' % (x, y, outfile))
 	
 	shutil.rmtree(tmp_dir)
 	
 	print "%s\t->\t%s\t->\t%s" % (infile, tmpfile, outfile)
 
+def call(cmd):
+	return subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
 
 
-# get command line arguments
-parser = argparse.ArgumentParser(prog="\033[1mready-for-bridge.py\033[0m", description="Convertes a folder with images to another folder to be ready for Camera Raw:\n\t1. Deconstructes all layers and chooses the one with biggest file size (so preview layers will be ignored)\n\t2. removes all layers aka flatten the image\n\t3. removes the alpha cannel\n\t4. writes Exif.Photo Dimension metadata to the image)", formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument("-i", "--infolder", help="input folder with image files to be converted", required=True)
-parser.add_argument("-o", "--outfolder", help="output folder to put the converted files in", required=True)
+def main():
+	# get command line arguments
+	parser = argparse.ArgumentParser(prog="\033[1mready-for-bridge.py\033[0m", description="Convertes images to to be ready for Camera Raw:\n\t1. Deconstructes all layers and chooses the one with biggest file size (so preview layers will be ignored)\n\t2. removes all layers aka flatten the image\n\t3. removes the alpha cannel\n\t4. writes Exif.Photo Dimension metadata to the image)", formatter_class=argparse.RawTextHelpFormatter)
 
-args = parser.parse_args()
+	parser.add_argument("-i", "--input", help="input file/folder to be converted", required=True)
+	parser.add_argument("-o", "--output", help="output file/folder to be written", required=True)
+	parser.add_argument("-b", "--batch", help="input and output are folders -- batch convert all files (default: convert single file)", action='store_true')
 
-# check everything is ok
-if subprocess.check_output("which convert", shell=True) == "":
-	raise Error("convert from ImageMagick must be installed... (http://www.imagemagick.org/)")
-if subprocess.check_output("which exiv2", shell=True) == "":
-	raise Error("exiv2 must be installed... (http://www.exiv2.org/)")
-if subprocess.check_output("which exiv2", shell=True) == "":
-	raise Error("sips must be installed... (https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/sips.1.html)")
-if args.outfolder == args.infolder:
-	raise Error("infolder must be different from outfolder...")
+	args = parser.parse_args()
 
-# run over all files
-for file in glob.glob(args.infolder + "/*"):
-	newfile = args.outfolder + "/" + os.path.split(file)[1]
-	convert(os.path.abspath(file), os.path.abspath(newfile))
+	# check everything is ok
+	if call("which convert") == "":
+		raise RuntimeError("convert from ImageMagick must be installed... (http://www.imagemagick.org/)")
+	if call("which exiv2") == "":
+		raise RuntimeError("exiv2 must be installed... (http://www.exiv2.org/)")
+	if call("which exiv2") == "":
+		raise RuntimeError("sips must be installed... (https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man1/sips.1.html)")
+	if args.output == args.input:
+		raise RuntimeError("infolder must be different from outfolder...")
+
+	if args.batch:
+		# run over all files
+		for file in glob.glob(args.input + "/*"):
+			newfile = args.output + "/" + os.path.split(file)[1]
+			convert(os.path.abspath(file), os.path.abspath(newfile))
+	else:
+		convert(args.input, args.output)
+
+if __name__ == "__main__":
+    main()
